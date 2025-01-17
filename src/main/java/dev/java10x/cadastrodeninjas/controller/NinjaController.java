c848e1e0823cec15658a1ed2c8bf72d0d269d043
Igor Pardinho <pardinhoigor@gmail.com>
2025-01-17 17:58:57 -0300

    feat: criação dos métodos get,post,put no NinjaController

--- src/main/java/dev/java10x/cadastrodeninjas/controller/NinjaController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/controller/NinjaController.java
@@ -5,12 +5,10 @@ import dev.java10x.cadastrodeninjas.model.NinjaModel;
 import dev.java10x.cadastrodeninjas.service.NinjaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
@@ -23,9 +21,22 @@ public class NinjaController {
 
     @GetMapping
     public ResponseEntity<List<NinjaModel>> findAll() {
-
         return ResponseEntity.status(HttpStatus.OK).body(ninjaService.findAll());
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<NinjaModel> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(ninjaService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<NinjaModel> save(@RequestBody NinjaModel ninja) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ninjaService.save(ninja));
+    }
+
+    @PutMapping
+    public ResponseEntity<NinjaModel> update(@RequestBody NinjaModel ninja) {
+        return ResponseEntity.status(HttpStatus.OK).body(ninjaService.update(ninja));
     }
 
 }
