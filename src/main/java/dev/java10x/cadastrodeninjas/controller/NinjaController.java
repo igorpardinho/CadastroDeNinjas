166ffe4effd3074ef591feece6cc87d72952941b
Igor Pardinho <pardinhoigor@gmail.com>
2025-01-17 17:40:11 -0300

    fix: correção da privacidade do método

--- src/main/java/dev/java10x/cadastrodeninjas/controller/NinjaController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/controller/NinjaController.java
@@ -1,16 +1,31 @@
 package dev.java10x.cadastrodeninjas.controller;
 
 
+import dev.java10x.cadastrodeninjas.model.NinjaModel;
+import dev.java10x.cadastrodeninjas.service.NinjaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
-    @GetMapping("/boasVindas")
-    public String boasVindas() {
-        return "Boas vindas";
+    private final NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<NinjaModel>> findAll() {
+
+        return ResponseEntity.status(HttpStatus.OK).body(ninjaService.findAll());
+
+    }
+
 }
